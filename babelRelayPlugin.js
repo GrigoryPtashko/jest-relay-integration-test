72f6a74c8eb3a798006d0e2ca74f3787163e46c2
Grigory Ptashko <grigory.ptashko@gmail.com>
2016-07-25 13:36:42 +0300

    Added outer graphql endpoint

--- babelRelayPlugin.js
+++ babelRelayPlugin.js
@@ -1,7 +1,7 @@
 var babelRelayPlugin   = require('babel-relay-plugin');
 var request            = require('sync-request');
 
-var url = 'http://localhost:10000/q';
+var url = 'http://hashnews.cfapps.io/q';
 
 var response = request('POST', url, {
   json: { query: "query IntrospectionQuery {__schema {queryType { name } mutationType { name } subscriptionType { name } types {...FullType } directives {name description args {...InputValue } onOperation onFragment onField } } } fragment FullType on __Type {kind name description fields(includeDeprecated: true) {name description args {...InputValue } type {...TypeRef } isDeprecated deprecationReason } inputFields {...InputValue } interfaces {...TypeRef } enumValues(includeDeprecated: true) {name description isDeprecated deprecationReason } possibleTypes {...TypeRef } } fragment InputValue on __InputValue {name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type {kind name ofType {kind name ofType {kind name ofType {kind name } } } }" },
