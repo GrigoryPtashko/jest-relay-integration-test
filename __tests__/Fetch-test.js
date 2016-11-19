17a67b1ce4634ddd48f8fd67038943259033fba9
Grigory Ptashko <grigory.ptashko@gmail.com>
2016-11-20 02:08:24 +0300

    Working fetch() test. Added `setupFiles` section in `package.json` for `Jest`

--- __tests__/Fetch-test.js
+++ __tests__/Fetch-test.js
@@ -1,16 +1,12 @@
 import 'whatwg-fetch';
 
 it('fetches data', async () => {
-  try {
-    const response = await fetch('http://hashnews.cfapps.io');
+  const response = await fetch('http://hashnews.cfapps.io');
 
-    console.log('fetch response', response);
+  // console.log('fetch response', response);
 
-    const data = await response.text();
+  const data = await response.text();
 
-    expect(data).not.toBe(null);
-  } catch (e) {
-    console.log('Error', e);
-  }
+  expect(data).not.toBe(null);
 });
 
